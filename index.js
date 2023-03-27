7335b527012c70889b850bb8297b514de700e443
ortegadriian <adrian.o.b.2030@gmail.com>
2023-03-27 20:45:37 +0200

    commit final

--- index.js
+++ index.js
@@ -28,43 +28,97 @@ btnNetejaEstil.onclick = netejaEstil;
 btnCarregaJSON.onclick = carregaJSON;
 btnNetejaLlista.onclick = netejaLlista;
 
+//
+const main = document.getElementById("principal");
+
 
 
 //Exercici 1: suma d'hores
 function calculaHora() {
-    inputTime1 = inputTime1
 
-    resultatTime.innerHTML = inputTime1[0];
-    
+    if (inputTime1.value && inputTime2.value) {
+        const separarHoras1 = inputTime1.value.split(':')
+        const separarHoras2 = inputTime2.value.split(':')
+
+        let hora1 = parseInt(separarHoras1[0])
+        let hora2 = parseInt(separarHoras2[0])
+
+        let minutos1 = parseInt(separarHoras1[1])
+        let minutos2 = parseInt(separarHoras2[1])
+
+            let horas = hora1 + hora2;
+            let minutos = minutos1 + minutos2;
+
+            if (minutos >= 60) {
+                minutos = minutos - 60;
+                horas++
+            }
+
+            if (horas < 10) {
+                horas = '0' + horas;
+            }
+
+            if (minutos < 10) {
+                minutos = '0' + minutos;
+            }
+
+            resultatTime.innerHTML = horas + ':' + minutos;
+    } else alert("Rellena los campos del ejercicio incompletos")
 }
 
+
+
 function netejaEx1() {
-    
-    
+    inputTime1.value = "00:00";
+    inputTime2.value = "00:00";
+    resultatTime.innerHTML = "";
+
+
 }
 
 // Exercici 2: Arrays
 function generaRandomArray() {
-    var numberAleatorio = Math.floor((Math.random() * (100 - 1 + 1)) +1);
-    var numbers = [];
-    for (var i = 0; i < 10; i++) {
-        numbers += numbers[i];
+    let miarray = Array.from({ length: 10 }, () => Math.floor(Math.random() * (100)));
+    let max = 0;
+    miarray.forEach(function (item, index, arr) {
+        if (item > max) {
+            max = item;
+        }
+        console.log(miarray);
+        resultatArray.innerHTML = miarray + " | El numero mas grande es: " + max;
+    });
 
-    }
-    resultatArray.innerHTML = numberAleatorio;
 }
 
 function netejaEx2() {
-    
+    miarray = [];
+    resultatArray.innerHTML = "";
+    console.log(miarray);
 }
 
 // Exercici 3: Estils CSS
 function canviaEstil() {
-    
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    main.style.borderColor = bgColor;
+
+    var x1 = Math.floor(Math.random() * 256 - 1);
+    var y2 = Math.floor(Math.random() * 256 - 1);
+    var z3 = Math.floor(Math.random() * 256 - 1);
+    var bgColor2 = "rgb(" + x1 + "," + y2 + "," + z3 + ")";
+    main.style.backgroundColor = bgColor2;
+
+    if (x <= 30 && y <= 30 && z <= 30) {
+        main.style.color = 'white';
+    }
 }
 
 function netejaEstil() {
-    
+    main.style.backgroundColor = 'white';
+    main.style.borderColor = 'black';
+    main.style.color = 'black';
 }
 
 // Exercici 4: JSON
@@ -81,10 +135,10 @@ function carregaJSON() {
         });
 }
 
-function pintaDadesJSON(data){
-    
+function pintaDadesJSON(data) {
+
 }
 
-function netejaLlista(){
-   
+function netejaLlista() {
+
 }
